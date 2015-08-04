4ac4c7783f16b28d0c18a1f4cd8ec96a0dc200c1
zhengna <zhengna@zhengnadeMac.local>
2015-08-04 00:26:03 -0700

    chapter20-2

--- Homepwner/ItemCell.m
+++ Homepwner/ItemCell.m
@@ -8,10 +8,66 @@
 
 #import "ItemCell.h"
 
+@interface ItemCell ()
+
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *imageViewHeightConstraint;
+//@property (nonatomic, weak) IBOutlet NSLayoutConstraint *imageViewWidthConstraint;
+
+@end
+
 @implementation ItemCell
+
+- (void)updateInterfaceForDynamicTypeSize{
+
+    UIFont *font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    UIFont *font_small = [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote];
+    self.nameLable.font = font;
+    self.serialNumberLabel.font = font_small;
+    self.valueLabel.font = font;
+    
+    static NSDictionary *imageSizeDictionary;
+    
+    if (!imageSizeDictionary) {
+        imageSizeDictionary = @{ UIContentSizeCategoryExtraSmall: @40,
+                                 UIContentSizeCategorySmall: @40,
+                                 UIContentSizeCategoryMedium: @40,
+                                 UIContentSizeCategoryLarge: @40,
+                                 UIContentSizeCategoryExtraLarge: @45,
+                                 UIContentSizeCategoryExtraExtraLarge: @55,
+                                 UIContentSizeCategoryExtraExtraExtraLarge: @65};
+    }
+    
+    NSString *userSize = [[UIApplication sharedApplication] preferredContentSizeCategory];
+    
+    NSNumber *imageSize = imageSizeDictionary[userSize];
+    self.imageViewHeightConstraint.constant = imageSize.floatValue;
+    //self.imageViewWidthConstraint.constant = imageSize.floatValue;
+}
 
 - (void)awakeFromNib {
     // Initialization code
+    [self updateInterfaceForDynamicTypeSize];
+    
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc addObserver:self
+           selector:@selector(updateInterfaceForDynamicTypeSize)
+               name:UIContentSizeCategoryDidChangeNotification
+             object:nil];
+    
+    NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self.thumbnailView
+                                                                  attribute:NSLayoutAttributeHeight
+                                                                  relatedBy:NSLayoutRelationEqual
+                                                                     toItem:self.thumbnailView
+                                                                  attribute:NSLayoutAttributeWidth
+                                                                 multiplier:1
+                                                                   constant:0];
+    [self.thumbnailView addConstraint:constraint];
+}
+
+- (void)dealloc{
+
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc removeObserver:self];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
--- Homepwner/ItemViewController.m
+++ Homepwner/ItemViewController.m
@@ -36,12 +36,22 @@
                                                                              action:@selector(addNewItem:)];
         
         navItem.rightBarButtonItem = bbi;
-        
         navItem.leftBarButtonItem = self.editButtonItem;
         
+        NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+        [nc addObserver:self
+               selector:@selector(updateTableViewForDynamicTypeSize)
+                   name:UIContentSizeCategoryDidChangeNotification
+                 object:nil];
     }
     
     return self;
+}
+
+- (void)dealloc{
+    
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc removeObserver:self];
 }
 
 - (void)addNewItem:(id)sender{
@@ -205,6 +215,28 @@
 - (void)viewWillAppear:(BOOL)animated{
     
     [super viewWillAppear:animated];
+    //[self.tableView reloadData];
+    [self updateTableViewForDynamicTypeSize];
+}
+
+- (void)updateTableViewForDynamicTypeSize{
+
+    static NSDictionary *cellHeightDictionary;
+    
+    if (!cellHeightDictionary) {
+        cellHeightDictionary = @{UIContentSizeCategoryExtraSmall: @44,
+                                 UIContentSizeCategorySmall: @44,
+                                 UIContentSizeCategoryMedium: @44,
+                                 UIContentSizeCategoryLarge: @44,
+                                 UIContentSizeCategoryExtraLarge: @55,
+                                 UIContentSizeCategoryExtraExtraLarge: @65,
+                                 UIContentSizeCategoryExtraExtraExtraLarge: @75 };
+    }
+    
+    NSString *userSize = [[UIApplication sharedApplication] preferredContentSizeCategory];
+    
+    NSNumber *cellHeight = cellHeightDictionary[userSize];
+    [self.tableView setRowHeight:cellHeight.floatValue];
     [self.tableView reloadData];
 }
 
